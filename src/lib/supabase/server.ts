4cd6b53d6a6259ca43d1cc23c1fe829def53c465
Lucky Morifi <NOKOLUCKY19@gmail.com>
2025-09-15 13:13:20 +0000

    This error is still not fixed. the .env.local looks like below
    NEXT_PUB

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // This is a server-only client
 export const createServerClient = () => {
-    const supabaseUrl = process.env.SUPABASE_URL!
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
     const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
     return createClient(supabaseUrl, supabaseServiceRoleKey, {
         auth: {
